1947d88c2925b5faaf7e29ea7aab2b5079b2eab1
Tony Kung <lps.tony@gmail.com>
2016-08-17 19:45:00 -0500

    add overflown alert feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ export default class TweetBox extends Component {
   constructor (props){
     super(props); 
     this.state = {
-      text: ""
+      text: "",
+      photoAdd: false
     }
   }  
 
@@ -14,12 +15,47 @@ export default class TweetBox extends Component {
     this.setState({ text: e.target.value});
   }
 
+  togglePhoto = () => {
+    this.setState({ photoAdd: !this.state.photoAdd });
+  }
+
+  handleCharCount = () => {
+    if(this.state.photoAdd){
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  }
+
+  handleOverflow = () => {
+    if(this.handleCharCount() < 0){
+      if (this.state.photoAdded) {
+        let beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        let overflowText = this.state.text.substring(140 - 23);
+      } else {
+        let beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        let overflowText = this.state.text.substring(140);
+      }
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! This sentence is too long!</strong>
+          &nbsp;...{beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  }
+
   render() {
     return (
       <div className="well clearfix">
+      {this.handleOverflow()}
         <textarea onChange={this.handleChange} className="form-control" ></textarea><br/>
-        <span>{140 - this.state.text.length}</span>
-        <button disabled={this.state.text.length === 0} className="btn btn-primary pull-right">Tweet</button>
+        <span>{this.handleCharCount()}</span>
+        <button disabled={this.handleCharCount() === 140} className="btn btn-primary pull-right">Tweet</button>
+        <button onClick={this.togglePhoto} className="btn btn-default pull-right">{this.state.photoAdd ? "✓ Photo Added" : "Add Photo" }</button>
       </div>
     );
   }
